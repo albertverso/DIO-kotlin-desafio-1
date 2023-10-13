d6c2370bf3b60afb9235eed7255daf229d1eef5e
Carlos Alberto <117851186+albertverso@users.noreply.github.com>
2023-10-12 21:28:02 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,57 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val id: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>,val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun printListaInscritos(){
+        for(alunos in inscritos){
+            println(alunos)
+        }
+    }
+    
+    fun printGradeFormacao(){
+        for(cont in conteudos){
+            println("${cont.nome} = ${cont.duracao}hrs")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+	val moduloJava = ConteudoEducacional("Curso Java", 260, Nivel.DIFICIL)
+    val moduloKotlin = ConteudoEducacional("Curso kotlin", 200, Nivel.INTERMEDIARIO)
+    
+    val listaConteudos = mutableListOf<ConteudoEducacional>()
+    listaConteudos.add(moduloJava)
+    listaConteudos.add(moduloKotlin)
+    
+    val carlos = Usuario("carlos", "1")
+    val debora = Usuario("debora", "2")
+    
+    val formacaoBackEnd = Formacao("BackEnd", listaConteudos, Nivel.DIFICIL)
+    
+    formacaoBackEnd.matricular(carlos)
+    formacaoBackEnd.matricular(debora)
+    
+    println("======================================")
+    println("Dados da formação")
+    println("${formacaoBackEnd.nome} - ${formacaoBackEnd.nivel}")
+    println("Grade formação")
+    println(formacaoBackEnd.printGradeFormacao())
+    println("=======================================")
+    println("Lista de inscritos")
+    println(formacaoBackEnd.printListaInscritos())
+   
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
